7ce155bb600c81f133d4dd5a74fd1f7e2e8eb0f6
alexey_naidiuk <alexeynaidiuk@gmail.com>
2023-02-22 15:25:04 +0200

    fixed projects

---
+++ scripts/inbox/true/stolpersteine.py
@@ -0,0 +1,61 @@
+import requests
+
+headers = {
+    'authority': 'www.stolpersteine-berlin.de',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+    'cache-control': 'max-age=0',
+    'origin': 'https://www.stolpersteine-berlin.de',
+    'referer': 'https://www.stolpersteine-berlin.de/en/contact',
+    'sec-ch-ua': '"Chromium";v="110", "Not A(Brand";v="24", "Google Chrome";v="110"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
+}
+s = 'Your message has been sent.'
+from os.path import basename
+
+import requests
+
+from module import Spam
+
+
+class ConcreteSpam(Spam):
+
+    def post(self, target) -> requests.Response | None:
+        data = {
+            'name': 'test',
+            'email': target,
+            'subject': self.get_text(),
+            'category': 'general',
+            'message': self.get_text(),
+            'send_copy': '1',
+            'gdpr_consent': '1',
+            'captcha_sid': '5510',
+            'captcha_token': '0XjmfeoPgdUcj3v0lRxmcwnNAjMTl9MslKFJuZWMjEE',
+            'captcha_response': 'planet',
+            'op': 'Send Message',
+            'form_build_id': 'form-s7lSk833ISjxfCaZrKs5vNvlpJQvajRJuSqHjrkEbIc',
+            'form_id': 'webform_submission_kontaktformular_node_220_add_form',
+        }
+
+        response = requests.post('https://www.stolpersteine-berlin.de/en/contact', headers=headers, data=data,
+                                 proxies=self.get_proxies())
+
+        return response
+
+
+def main():
+    spam = ConcreteSpam(basename(__file__).removesuffix('.py'), s)
+    res = spam.send_post()
+    if res:
+        spam.run_concurrently()
+
+
+if __name__ == '__main__':
+    main()
