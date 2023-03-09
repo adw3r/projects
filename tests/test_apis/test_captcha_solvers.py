84c82a6b6c5aaabd10547ebe8d6773f8eb45cd03
alexey_naidiuk <alexeynaidiuk@gmail.com>
2023-03-09 14:05:01 +0200

    fixed projects

--- tests/test_apis/test_captcha_solvers.py
+++ tests/test_apis/test_captcha_solvers.py
@@ -6,10 +6,8 @@ from module.apis.captcha_solvers import CapMonsterSolver
 class TestCapMonsterSolver(TestCase):
 
     def test_solving(self):
-        self.skipTest('not needed')
         solver: CapMonsterSolver = CapMonsterSolver()
-
-        googlesitekey = '6Le-wvkSAAAAAPBMRTvw0Q4Muexq9bi0DJwx_mJ-'
-        pageurl = 'https://www.google.com/recaptcha/api2/demo'
-        result: str | None = solver.solve(googlekey=googlesitekey, pageurl=pageurl, time_limit=10, version='v2')
+        googlekey = '6LcPudEjAAAAAHiEiPG17P7u3yFumpmj60UjLA9U'
+        pageurl = 'https://app.txtwizard.com/registration'
+        result: str | None = solver.solve(googlekey=googlekey, pageurl=pageurl)
         self.assertIn('03A', result)
